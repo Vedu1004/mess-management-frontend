87e4b2c0e6239f17ce6ec1f55407f4e60ced979f
VEDANT JOSHI <128678703+Vedu1004@users.noreply.github.com>
2024-07-17 19:35:54 +0530

    Update axios.js

--- messmate/src/Api/axios.js
+++ messmate/src/Api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3001";
+const BASE_URL = "https://mess-management-for-universities.onrender.com";
 
 export default axios.create({
   baseURL: BASE_URL,
